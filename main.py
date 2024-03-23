d40ab3884bd4ca13dc38a357c43acbc2a4c146d4
Victor Hugo Dias de Oliveira <victorhugodias2001@gmail.com>
2024-03-23 17:26:33 -0300

    Criada as funcoes para encriptar e decriptar a chave que encripta os nomes

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from src.utils import (
     verify_if_path_exists,
-    clear_console,
+    clear_console)
+from src.key import (
     generate_key,
     load_key)
-from src.encrypt_file import encrypt_and_delete_files_in_directory
-from src.decrypt_file import decrypt_and_delete_files_in_directory
+from src.encrypt_file import (
+    encrypt_and_delete_files_in_directory,
+    encrypt_and_delete_file)
+from src.decrypt_file import (
+    decrypt_and_delete_files_in_directory,
+    decrypt_and_delete_file)
 from src.encrypt_file_name import encrypt_file_names_in_directory
 from src.decrypt_file_name import decrypt_file_names_in_directory
 
 KEY_PATH = '/home/mamada/Documents/Secure/key.txt'
+ENCRYPTED_KEY_PATH = '/home/mamada/Documents/Secure/key.txt.enc'
 
 print('1 - Encriptar Arquivos')
 print('2 - Decriptar Arquivos')
 print('3 - Encriptar Nomes')
 print('4 - Decriptar Nomes')
 print('5 - Gerar chave')
+print('6 - Encriptar chave')
+print('7 - Decriptar chave')
 decision = input('-> ')
 
 match decision:
@@ -64,6 +72,24 @@ match decision:
             print('Chave ja existente.')
         else:
             generate_key(KEY_PATH)
+
+    case '6':
+        clear_console()
+        key = input("Digite a chave AES (16, 24 ou 32 bytes): ").encode('utf-8')
+        if len(key) not in [16, 24, 32]:
+            print("A chave precisa ter 16, 24 ou 32 bytes.")
+        else:
+            encrypt_and_delete_file(KEY_PATH, key)
+            print("Chave criptografada com sucesso!")
+
+    case '7':
+        clear_console()
+        key = input("Digite a chave AES (16, 24 ou 32 bytes): ").encode('utf-8')
+        if len(key) not in [16, 24, 32]:
+            print("A chave precisa ter 16, 24 ou 32 bytes.")
+        else:
+            decrypt_and_delete_file(ENCRYPTED_KEY_PATH, key)
+            print("Chave criptografada com sucesso!")
 
     case _:
         print('Opcao invalida, finalizando programa...')
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,5 @@
 import os
 import sys
-from cryptography.fernet import Fernet
 
 def verify_if_path_exists(path):
     if os.path.exists(path):
@@ -9,13 +8,3 @@ def verify_if_path_exists(path):
 
 def clear_console():
     os.system('clear')
-
-def generate_key(key_path):
-    with open(key_path, 'wb') as f:
-        f.write(Fernet.generate_key())
-    print('Chave gerada com sucesso.')
-
-def load_key(key_file):
-    if os.path.exists(key_file):
-        with open(key_file, 'rb') as f:
-            return f.read()
